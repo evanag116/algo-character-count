e0dd88c723af87e739ea3f481bf841832edb56d2
Evan Garcia <evanag116@yahoo.com>
2022-06-05 19:43:11 -0500

    completed in py

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,15 @@
 def char_count(str):
-  # Your code here
+  ans_dict = {i: str.count(i) for i in set(str.replace(" ", ""))}
+  letters = sorted(ans_dict)
+  counts = sorted(ans_dict.values(), reverse=True)
+  res = []
+
+  for num in range(len(counts)):
+    for letter in letters:
+      if counts[num] == str.count(letter):
+        res.append([letter, counts[num]])
+        letters.remove(letter)
+        break
+
+  return res
+
